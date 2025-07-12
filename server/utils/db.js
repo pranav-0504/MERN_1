3ab3c7d11264d04dda19d8ae96023379a293c7ae
Pranav Aggarwal <pranav.aggarwal2004@gmail.com>
2025-07-12 19:41:20 +0530

    Fixed

--- server/utils/db.js
+++ server/utils/db.js
@@ -2,11 +2,8 @@ const mongoose = require("mongoose");
 
 // DataBase ka naam rkha h maine alg se mern_admin: iss link me:
 //* Mongo DB COMPASS Connect LINK:
-// // const URI = "mongodb+srv://pranavaggarwal2004:J6JpouKFEwE0RxzS@admin.5wrfdc5.mongodb.net/mern_admin";
 
 //! Driver Connect LINK:
-//* DataBase ka naam rkha h maine alg se mern_admin: iss link me:
-// // const URI = "mongodb+srv://pranavaggarwal2004:J6JpouKFEwE0RxzS@admin.5wrfdc5.mongodb.net/mern_Admin?retryWrites=true&w=majority&appName=Admin";
 
 const URI = process.env.MONGODB_URI;            // From .env file
 
